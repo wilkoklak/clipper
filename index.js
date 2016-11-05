3765f8c5b16f062463dfb8903abfabecef7cdc6b
Artur <thereaperv11@gmail.com>
2016-11-05 11:55:30 +0100

    Dodanie index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+// Zachowanie referencji do głównego okna, żeby garbage collector nie usunął
+// Według dokumentacji electrona
+var main;
+
+// Utworzenie głównego okna
+function createMain() {
+	main = new BrowserWindow({width: 800, height: 600});
+	main.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.htm'),
+		protocol: 'file:',
+		slashes: true
+	}))
+	main.on('closed', () => {
+		main = null;
+	})
+}
+
+// Gotowość aplikacji
+app.on('ready', createMain);
+
+// Wszystkie okna zamknięte
+app.on('window-all-closed', () => {
+	// Coś z macOS'em, nie wiem, nie posiadam :P
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+	// Także coś z macOS'em...
+  if (win === null) {
+    createWindow()
+  }
+})
